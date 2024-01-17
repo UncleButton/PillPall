806bc5cc399c1159fae6112f29395c70861ce6e7
Uncle Button <UncleButton@users.noreply.github.com>
2024-01-17 16:08:49 -0500

    NotificationBanner

---
+++ src/components/NotificationBanner.vue
@@ -0,0 +1,103 @@
+<template>
+  <transition name="fade">
+    <div v-if="isVisible" class="notification" :class="notificationType">
+      {{ text }}
+      <span class="close-btn" @click="handleCloseClick">×</span>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'NotificationBanner',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'info',
+      validator: value => ['info', 'error', 'warning', 'success'].includes(value),
+    },
+    autoHide: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+  computed: {
+    notificationType() {
+      return this.type === 'info' ? 'notification-info' : `notification-${this.type}`;
+    },
+  },
+  methods: {
+    handleCloseClick() {
+        this.isVisible = false;
+        this.$emit('close');
+    },
+  },
+  mounted() {
+    if (this.autoHide) {
+      setTimeout(() => {
+        this.isVisible = false;
+        this.$emit('close');
+      }, 10000); // 10 seconds before fading
+    }
+  },
+};
+</script>
+
+<style scoped>
+.notification {
+  position: relative;
+  top: 0;
+  
+  transform: translateX(-50%);
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close-btn {
+  position: absolute;
+  top: 0px;
+  right: 10px;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.notification-info {
+  background-color: #3498db; /* Blue background for info */
+  color: #fff;
+}
+
+.notification-error {
+  background-color: #e74c3c; /* Red background for error */
+  color: #fff;
+}
+
+.notification-warning {
+  background-color: #f39c12; /* Yellow background for warning */
+  color: #fff;
+}
+
+.notification-success {
+  background-color: #2ecc71; /* Green background for success */
+  color: #fff;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
